59513cca853474f385701d889835110b99caae87
Camelia Osan <osan.camelia2001@gmail.com>
2025-10-24 02:54:11 +0300

    + update files

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,7 +1,7 @@
 export default function Header() {
     return (
         <header>
-            <img src="../images/globe.png" alt="globe icon" />
+            <img src="/Travel-Journal/images/globe.png" alt="globe icon" />
             <h1 className="logo-title">My Travel Journal.</h1>
         </header>
     )
