97b2eddf684e440501fa3c23bf3222be853b5c64
Camelia Osan <osan.camelia2001@gmail.com>
2025-10-24 02:33:35 +0300

    + add files

---
+++ data.js
@@ -0,0 +1,86 @@
+export default [
+    {
+        id: 1,
+        img: {
+            src: "https://images.ctfassets.net/7mmwp5vb96tc/785pCQyLIL6bQ5q1gHTCLy/56ebf4142dd4fd68fe2b0c2f064a6b1b/Aurora_Borealis_Norway_HGR_147375_Photo_Getty_Images-_1_.jpg?q=40&w=3840&fm=webp",
+            alt: "Tromsø, Norway"
+        },
+        title: "Tromsø",
+        country: "Norway",
+        googleMapsLink: "https://maps.app.goo.gl/eUaVmcrRgqQTHr477",
+        activity: "Chase the Northern Lights",
+        text: "With long nights, clear skies, and plenty of winter activities, Tromsø, located 350km above the Arctic Circle beneath the Auroral Oval, is one of the best places on earth to witness the aurora borealis."
+    },
+    {
+        id: 2,
+        img: {
+            src: "https://gobargingwp-s3.s3.eu-west-1.amazonaws.com/wp-content/uploads/2022/12/Keukenhoff-Gardens-and-Windmill.jpg",
+            alt: "Amsterdam, The Netherlands"
+        },
+        title: "Keukenhof Garden, Lisse",
+        country: "The Netherlands",
+        googleMapsLink: "https://maps.app.goo.gl/NfrnMdsupwgXw4ss7",
+        activity: "Explore the Keukenhof tulip fields",
+        text: "The Keukenhof tulip fields offer endless trails among tulips of every hue imaginable. This flower oasis is not just a photo opportunity; it's a chance to explore themed gardens, art installations, and even take a boat tour through the flower-lined waterways."
+    },
+    {
+        id: 3,
+        img: {
+            src: "https://lp-cms-production.imgix.net/2025-04/Shutterstock1411811462.jpg?auto=format,compress&q=72&w=1440&h=810&fit=crop",
+            alt: "Madeira, Portugal"
+        },
+        title: "Madeira",
+        country: "Portugal",
+        googleMapsLink: "https://maps.app.goo.gl/BvUGoiNXGngwEKPZ9",
+        activity: "Hike the levada trails",
+        text: "Madeira is a stunning island known for its rugged landscape, lush vegetation, and beautiful levada trails. These irrigation channels offer some of the best hiking opportunities in Portugal, with paths that wind through the mountains and provide breathtaking views of the island's natural beauty."
+    },
+    {
+        id: 4,
+        img: {
+            src: "https://images.movehub.com/wp-content/uploads/2017/12/14094810/Edinburgh.jpg",
+            alt: "Edinburgh, Scotland"
+        },
+        title: "Edinburgh",
+        country: "Scotland",
+        googleMapsLink: "https://maps.app.goo.gl/6H96xD96jc6rhv819",
+        activity: "Explore the historic and haunted sites",
+        text: "Edinburgh is a city steeped in history and mystery, with its ancient castles, cobbled streets, and a rich tapestry of stories waiting to be discovered. From the iconic Edinburgh Castle to the eerie vaults beneath the city, there's no shortage of fascinating sites to explore."
+    },
+    {
+        id: 5,
+        img: {
+            src: "https://www.discoverwalks.com/blog/wp-content/uploads/2022/08/peles-castle-1.jpg",
+            alt: "Peleș Castle, Romania"
+        },
+        title: "Peleș Castle, Sinaia",
+        country: "Romania",
+        googleMapsLink: "https://maps.app.goo.gl/dyXSEE7F9f8xusvZ8",
+        activity: "Explore the stunning architecture and gardens",
+        text: "Peleș Castle is a masterpiece of Neo-Renaissance architecture nestled in the Carpathian Mountains. Surrounded by lush gardens and breathtaking scenery, this castle offers a glimpse into Romania's royal past and is a must-visit for history and architecture enthusiasts."
+    },
+    {
+        id: 6,
+        img: {
+            src: "https://www.dailyartmagazine.com/wp-content/uploads/2023/06/Alexandre_Brun_-_View_of_the_Salon_Carre_at_the_Louvre.jpg",
+            alt: "Louvre Museum, Paris"
+        },
+        title: "Louvre Museum, Paris",
+        country: "France",
+        googleMapsLink: "https://maps.app.goo.gl/xrXfNuxWwg8mjPTa6",
+        activity: "Visit the world's largest art museum",
+        text: "The Louvre Museum is a historic monument in Paris and one of the largest and most visited art museums in the world. Its glass pyramid entrance and vast collection of art, including the Mona Lisa and the Venus de Milo, make it a must-visit for art lovers and history enthusiasts alike."
+    },
+    {
+        id: 7,
+        img: {
+            src: "https://static.independent.co.uk/2024/01/02/11/newFile-2.jpg",
+            alt: "Sicily, Italy"
+        },
+        title: "Sicily, Italy",
+        country: "Italy",
+        googleMapsLink: "https://maps.app.goo.gl/D6sTMq8kHudTkYPQ7",
+        activity: "Explore the beautiful coastline and historic sites",
+        text: "Sicily is a Mediterranean island rich in history, culture, and natural beauty. From the ancient ruins of Agrigento to the stunning beaches of Taormina, Sicily offers a diverse range of experiences for travelers seeking both relaxation and adventure."
+    },
+]
